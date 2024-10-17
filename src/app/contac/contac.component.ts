75c7b47448773bb7f51d0a097dff5c920ed4ab0b
hasan.souliman@yolo.clinic <hasan.souliman@yolo.clinic>
2024-10-17 23:16:07 +0300

    changes

--- src/app/contac/contac.component.ts
+++ src/app/contac/contac.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ContacComponent implements OnInit {
   intersted=['BackEnd','FrontEnd','DataBase']
   constructor() { }
+  Email :string="hasansuliman50@gmail.com"
 
   ngOnInit(): void {
   }
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ProjectsComponent implements OnInit {
 
   constructor() { }
-
+Email :string="hasansuliman50@gmail.com"
   ngOnInit(): void {
   }
 
